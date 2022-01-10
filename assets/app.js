1326be0a4200a9a8b9fc88271a0a6d511738e29e
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-01-10 21:15:07 +0900

    alpinejs 플러그인을 추가합니다.

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,18 @@
 import Alpine from 'alpinejs'
-import * as params from '@params';
+import intersect from '@alpinejs/intersect'
+import persist from '@alpinejs/persist'
+import trap from '@alpinejs/trap'
+import collapse from '@alpinejs/collapse'
+import * as params from '@params'
 
 window.Alpine = Alpine
-window.params = params;
+window.params = params
+window.$ = document.querySelector.bind(document)
+window.$$ = document.querySelectorAll.bind(document);
 
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
+[intersect, persist, trap, collapse].forEach(item => Alpine.plugin(item))
+Alpine.start()
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('domready!', Alpine, params)
+  console.log('domready!', Alpine, params, $, $$)
 })
