cd520929c1aa1c40f04e2aa319567a1bca545ba4
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-01-19 15:54:15 +0900

    gsheet 정보 출력은 제거하고 요청 헤더에 시트 제목이 있으면 해당 시트를 사용합니다.

--- exampleSite/functions/gsheet.js
+++ exampleSite/functions/gsheet.js
@@ -1,26 +1,23 @@
 /**
  * Google spreadsheet
  *
- * GET /.netlify/functions/gsheet
  * POST /.netlify/functions/gsheet
  *
  * @see env-vars
  * GOOGLE_SERVICE_ACCOUNT_EMAIL=
  * GOOGLE_PRIVATE_KEY=
  * GOOGLE_SPREADSHEET_ID_FROM_URL=
+ *
+ * from https://github.com/sw-yx/netlify-google-spreadsheet-demo
  */
-
-// from https://github.com/sw-yx/netlify-google-spreadsheet-demo
 
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-// required env vars
 if (!process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL)
   throw new Error('no GOOGLE_SERVICE_ACCOUNT_EMAIL env var set');
 if (!process.env.GOOGLE_PRIVATE_KEY)
   throw new Error('no GOOGLE_PRIVATE_KEY env var set');
 if (!process.env.GOOGLE_SPREADSHEET_ID_FROM_URL)
-  // spreadsheet key is the long id in the sheets URL
   throw new Error('no GOOGLE_SPREADSHEET_ID_FROM_URL env var set');
 
 exports.handler = async (event, context) => {
@@ -31,60 +28,42 @@ exports.handler = async (event, context) => {
   });
   await doc.loadInfo();
 
-  const sheet = doc.sheetsByIndex[0];
-  // console.log('accessing', sheet.title, 'it has ', sheet.rowCount, ' rows');
-  const path = event.path.replace(/\.netlify\/functions\/[^/]+/, '');
-  const segments = path.split('/').filter((e) => e);
+  let sheet = doc.sheetsByIndex[0];
+  if ('sheet-title' in event.headers) {
+    let sheetTitle = event.headers['sheet-title'];
+    if (sheetTitle in doc.sheetsByTitle) {
+      sheet = doc.sheetsByTitle[sheetTitle];
+    }
+    else {
+      throw new Error('Sheet title "' + sheetTitle + '" not found.');
+    }
+  }
 
   try {
     switch (event.httpMethod) {
-      /* GET /.netlify/functions/gsheet */
-      case 'GET':
-        const rows = await sheet.getRows(); // can pass in { limit, offset }
-        const serializedRows = rows.map(serializeRow);
-        return {
-          statusCode: 200,
-          // body: JSON.stringify(rows) // dont do this - has circular references
-          body: JSON.stringify(serializedRows) // better
-        };
-      /* POST /.netlify/functions/gsheet */
       case 'POST':
-        /* parse the string body into a useable JS object */
         const data = JSON.parse(event.body);
-        // console.log('`POST` invoked', data);
         const addedRow = await sheet.addRow(data);
-        // console.log({ addedRow });
         return {
           statusCode: 200,
           body: JSON.stringify({
             message: `POST Success - added row ${addedRow._rowNumber - 1}`,
-            rowNumber: addedRow._rowNumber - 1 // minus the header row
+            rowNumber: addedRow._rowNumber - 1
           })
         };
-      /* Fallthrough case */
       default:
         return {
           statusCode: 500,
           body: 'unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE'
         };
     }
-  } catch (err) {
+  }
+  catch (err) {
     console.error('error ocurred in processing ', event);
     console.error(err);
     return {
       statusCode: 500,
       body: err.toString()
     };
-  }
-
-  /**
-   * utils
-   */
-  function serializeRow(row) {
-    let temp = {};
-    sheet.headerValues.map((header) => {
-      temp[header] = row[header];
-    });
-    return temp;
   }
 };
