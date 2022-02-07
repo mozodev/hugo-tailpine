b972cc4ed8e28444d01f937aef013f62d9abf9e2
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-02-07 12:54:46 +0900

    소소한 레이아웃 수정사항을 추가합니다.

--- assets/app.js
+++ assets/app.js
@@ -4,15 +4,19 @@ import persist from '@alpinejs/persist'
 import trap from '@alpinejs/trap'
 import collapse from '@alpinejs/collapse'
 import * as params from '@params'
+import { Iodine } from '@kingshott/iodine';
+
+const iodine = new Iodine();
 
 window.Alpine = Alpine
 window.params = params
 window.$ = document.querySelector.bind(document)
 window.$$ = document.querySelectorAll.bind(document);
+window.iodine = iodine;
 
 [intersect, persist, trap, collapse].forEach(item => Alpine.plugin(item))
 Alpine.start()
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('domready!', Alpine, params, $, $$)
+  console.log('dom ready!', params)
 })
