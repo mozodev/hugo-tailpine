be762e9786dce0f1774cbf4194a51421f1d7bc1a
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-02-26 17:05:28 +0900

    구글 웹폰트 params 지정하면 lighthouse 검사 통과할 수 있도록 관련 구문을 헤더에 출력합니다.

--- assets/_jsconfig/tailwind.config.js
+++ assets/_jsconfig/tailwind.config.js
@@ -1,10 +1,18 @@
-const baseDir = __dirname + '/../..';
+const baseDir = __dirname + '/../..'
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   darkMode: 'class',
 	content: [
     baseDir + '/layouts/**/*.html',
     baseDir + '/assets/**/*.{js,html}'
   ],
+  theme: {
+    fontFamily: {
+      sans: [ '"Noto Sans KR"', ...fontFamily.sans],
+      heading: [ '"Noto Serif KR"', ...fontFamily.serif]
+    },
+  },
 	plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
