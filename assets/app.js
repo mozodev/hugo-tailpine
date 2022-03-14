f57ff8d6b4568559cec47772478d6975f7b5416a
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-03-14 10:42:54 +0900

    jQuery 없이 $ 을 사용하기 위해 bling.js 를 추가합니다.

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,19 @@
 import * as params from '@params'
-
 window.params = params
-window.$ = document.querySelector.bind(document);
-window.$$ = document.querySelectorAll.bind(document);
-let find = Element.prototype.querySelectorAll;
-let each = Array.prototype.forEach;
-let on = Node.prototype.addEventListener;
+
+// https://blog.weirdx.io/post/14431
+// https://gist.github.com/paulirish/12fb951a8b893a454b32#file-bling-js
+window.$ = document.querySelectorAll.bind(document);
+Node.prototype.on = window.on = function (name, fn) {
+  this.addEventListener(name, fn);
+}
+NodeList.prototype.__proto__ = Array.prototype;
+NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
+  this.forEach(function (elem, i) {
+    elem.on(name, fn);
+  });
+}
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded')
+  console.log('DOMContentLoaded');
 })
